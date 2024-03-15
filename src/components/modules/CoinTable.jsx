7af4553ca98451e20d03ebabfcf5003d7bee5d12
Hamed Jahan <iamhamedjahan@gmail.com>
2024-03-15 21:48:31 +0330

    create coin table and style it

--- src/components/modules/CoinTable.jsx
+++ src/components/modules/CoinTable.jsx
@@ -1,8 +1,66 @@
-import React from "react";
+import { RotatingLines } from "react-loader-spinner";
 
-function CoinTable({ coins }) {
-  console.log(coins);
-  return <div>CoinTable</div>;
+import ChartUp from "../../assets/chart-up.svg";
+import ChartDown from "../../assets/chart-down.svg";
+
+import styles from "./CoinTable.module.css";
+
+function CoinTable({ coins, isLoading }) {
+  return (
+    <div className={styles.container}>
+      {isLoading ? (
+        <RotatingLines strokeColor="#3874ff" strokeWidth="2" />
+      ) : (
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th>Coin</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>24H</th>
+              <th>Total Volume</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {coins.map((coin) => (
+              <TableRow coin={coin} key={coin.id} />
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
 }
 
 export default CoinTable;
+
+const TableRow = ({
+  coin: {
+    name,
+    image,
+    current_price,
+    total_volume,
+    price_change_percentage_24h: price_change,
+  },
+}) => {
+  return (
+    <tr>
+      <td>
+        <div className={styles.symbol}>
+          <img src={image} alt={name} />
+          <span>{name.toUpperCase()}</span>
+        </div>
+      </td>
+      <td>{name}</td>
+      <td>${current_price.toLocaleString()}</td>
+      <td className={price_change > 0 ? styles.success : styles.error}>
+        {price_change.toFixed(2)}%
+      </td>
+      <td>{total_volume.toLocaleString()}</td>
+      <td>
+        <img src={price_change > 0 ? ChartUp : ChartDown} alt={name} />
+      </td>
+    </tr>
+  );
+};
--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -5,18 +5,20 @@ import { getCoinList } from "../../services/CryptoApi";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const getData = async () => {
       const res = await fetch(getCoinList());
       const json = await res.json();
       setCoins(json);
+      setIsLoading(false);
     };
     getData();
   }, []);
   return (
     <div>
-      <CoinTable coins={coins} />
+      <CoinTable coins={coins} isLoading={isLoading} />
     </div>
   );
 }
