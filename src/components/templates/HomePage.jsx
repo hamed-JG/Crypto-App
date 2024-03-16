1d88a6e6e9b0a5cb538f9a4023108a05e14bc4b4
Hamed Jahan <iamhamedjahan@gmail.com>
2024-03-16 10:34:36 +0330

    create pagination and style it

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,24 +1,29 @@
 import { useEffect, useState } from "react";
+
 import CoinTable from "../modules/CoinTable";
+import Pagination from "../modules/Pagination";
 
 import { getCoinList } from "../../services/CryptoApi";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
+    setIsLoading(true);
     const getData = async () => {
-      const res = await fetch(getCoinList());
+      const res = await fetch(getCoinList(page));
       const json = await res.json();
       setCoins(json);
       setIsLoading(false);
     };
     getData();
-  }, []);
+  }, [page]);
   return (
     <div>
       <CoinTable coins={coins} isLoading={isLoading} />
+      <Pagination page={page} setPage={setPage} />
     </div>
   );
 }
